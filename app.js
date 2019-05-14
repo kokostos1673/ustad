eaefcf161dda3c3d0cc8e62b594692ac19f368cf
kokostos1673 <kt1673@nyu.edu>
2019-05-15 00:10:31 +0400

    Final update

--- app.js
+++ app.js
@@ -193,6 +193,6 @@ app.get('*', function(req, res){
 });
 
 
-//const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+//const PORT = 8080;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
